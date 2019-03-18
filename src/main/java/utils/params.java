b5fb241fe46217d4ebf6531b29efba97d6f04a62
Chris Emerson <cme210@yahoo.com>
2019-03-18 17:27:22 -0400

    Aetna

--- src/main/java/utils/params.java
+++ src/main/java/utils/params.java
@@ -8,10 +8,10 @@ public class params {
     public static String GRID_URL = "http://localhost:4444/wd/hub";
 
 
-    public static String BATCH_NAME = "Magic Leap 02 VG S";
-    public static String APP_NAME = "Magic Leap  02";
-    public static String TEST_NAME = "Magic Leap  02";
-    public static String URL_FILE = "MagicLeap.csv";
+    public static String BATCH_NAME = "Aetna 01 VG";
+    public static String APP_NAME = "Aetna";
+    public static String TEST_NAME = "Aetna";
+    public static String URL_FILE = "Aetna.csv";
 
     public static MatchLevel MATCH_MODE = MatchLevel.STRICT;
     public static String BATCH_ID = null;
