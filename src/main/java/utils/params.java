a320f999030f7109871f1d032cecd31c952309f1
Chris Emerson <cme210@yahoo.com>
2019-04-25 08:40:02 -0400

    u

--- src/main/java/utils/params.java
+++ src/main/java/utils/params.java
@@ -5,7 +5,8 @@ import com.applitools.eyes.MatchLevel;
 public class params {
 
     public static String EYES_KEY = System.getProperty("eyesAPIKey", "DEFAULT_TOKEN");
-    public static String GRID_URL = "http://localhost:4444/wd/hub";
+    public static String GRID_URL = "zdmoZglAeFpz109AF1maELY9IdnyazVFMrGq1jduomN2U110";
+            // CME "http://localhost:4444/wd/hub";
 
     private static String name = "Carnival";
     public static Boolean changePage = false;  // to change the content for demo purposes
