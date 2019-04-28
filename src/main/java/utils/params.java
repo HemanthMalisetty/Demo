f57c1f655b1435cdf8f409ce73abfb58f2678268
Chris Emerson <cme210@yahoo.com>
2019-04-28 19:30:22 -0400

    Block FF

--- src/main/java/utils/params.java
+++ src/main/java/utils/params.java
@@ -8,20 +8,34 @@ public class params {
     public static String GRID_URL = "http://localhost:4444/wd/hub";
 
     private static String name = "url";
+    private static String suffix = " DG01";
     public static Boolean changePage = false;  // to change the content for demo purposes
-
-    //public static String BATCH_NAME = name + " Jenkins 01";
-    public static String BATCH_NAME = System.getenv("APPLITOOLS_BATCH_NAME");
-
-    public static String APP_NAME = name + " 01";
-    public static String TEST_NAME = name + " VG 03";
+    public static String APP_NAME = name + suffix;
+    public static String TEST_NAME = name + suffix;
     public static String URL_FILE = name + ".csv";
-
     public static MatchLevel MATCH_MODE = MatchLevel.LAYOUT;
-    public static String BATCH_ID = System.getenv("APPLITOOLS_BATCH_ID");
-
     public static Boolean FULL_SCREEN = true;
-
     public static Boolean DISABLE_EYES = false;
+
+    public static String BATCH_NAME = getBatchName();
+    public static String BATCH_ID = getBatchId();
+
+    private static String getBatchName(){
+        String batchName = name + suffix;
+        String envName = System.getenv("APPLITOOLS_BATCH_NAME");
+
+        if(envName != null) batchName = envName;
+
+        return batchName;
+    }
+
+    private static String getBatchId(){
+        String batchId = null;
+
+        if(System.getenv("APPLITOOLS_BATCH_ID") != null)
+            batchId = System.getenv("APPLITOOLS_BATCH_ID");
+
+        return batchId;
+    }
 }
 
