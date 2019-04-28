bf3ea3aa101c9e47ecfabb7fa2ce1cd62405ee15
Chris Emerson <cme210@yahoo.com>
2019-04-28 18:28:40 -0400

    Jenkins plug-in batching

--- src/main/java/utils/params.java
+++ src/main/java/utils/params.java
@@ -10,13 +10,15 @@ public class params {
     private static String name = "url";
     public static Boolean changePage = false;  // to change the content for demo purposes
 
-    public static String BATCH_NAME = name + " Jenkins 01";
+    //public static String BATCH_NAME = name + " Jenkins 01";
+    public static String BATCH_NAME = System.getenv("APPLITOOLS_BATCH_NAME")
+
     public static String APP_NAME = name + " 01";
     public static String TEST_NAME = name + " VG 03";
     public static String URL_FILE = name + ".csv";
 
     public static MatchLevel MATCH_MODE = MatchLevel.LAYOUT;
-    public static String BATCH_ID = "jenkins123";
+    public static String BATCH_ID = System.getenv("APPLITOOLS_BATCH_ID");
 
     public static Boolean FULL_SCREEN = true;
 
