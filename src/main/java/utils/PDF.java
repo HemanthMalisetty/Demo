1b54f1fae5730cd67117ba0935788ee0141182f0
Chris Emerson <cme210@yahoo.com>
2019-11-12 14:15:19 -0500

    Update PDF.java

--- src/main/java/utils/PDF.java
+++ src/main/java/utils/PDF.java
@@ -16,8 +16,7 @@ import java.io.IOException;
 public class PDF {
 
     public static void main (String args[]) throws IOException {
-        //Creating PDF document object
-        PDDocument document = new PDDocument();
+
 
         String inputImageFullName = "log/diffs/url VG Demo/url VG Demo/1200x1040/Mac OS X 10.14/Chrome/00000251829776547219/00000251829776546950/step 1 https~www.timeanddate.com~countdown~newyear-Diff.png";
         double scaleFactor = .250;
@@ -25,9 +24,15 @@ public class PDF {
         try {
             // https://pdfbox.apache.org/docs/2.0.8/javadocs/overview-summary.html
 
+            //Creating PDF document object
+            PDDocument document = new PDDocument();
+
             //Creating a blank page
             PDPage page = new PDPage();
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            float pageHeight = page.getMediaBox().getHeight();
+            float pageWidth = page.getMediaBox().getWidth();
 
             //Figure out where to put the image (upper left)
 
@@ -36,21 +41,28 @@ public class PDF {
             BufferedImage inputImage = ImageIO.read(inputFile);
             int imgWidth = inputImage.getWidth(null);
             int imgHeight = inputImage.getHeight(null);
+
+            //Figure out the scale factor based on width to fit 2 images side by side
+            scaleFactor = ((pageWidth / 2.5) / imgWidth);
+
+
             long scaledWidth = Math.round( (double) imgWidth * scaleFactor);
             long scaledHeight = Math.round( (double) imgHeight * scaleFactor);
             PDImageXObject pdImage = PDImageXObject.createFromFile(inputImageFullName,document);
-            contentStream.drawImage(pdImage, 70, 400, scaledWidth, scaledHeight);
+
+            contentStream.drawImage(pdImage, 50, pageHeight - (scaledHeight + 45), scaledWidth, scaledHeight);
+            contentStream.drawImage(pdImage, 55 + scaledWidth, pageHeight - (scaledHeight + 45), scaledWidth, scaledHeight);
 
             //Begin the Content stream
             contentStream.beginText();
 
             //Setting the font to the Content stream
-            contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+            contentStream.setFont(PDType1Font.HELVETICA, 12);
 
             //Setting the position for the line
-            contentStream.newLineAtOffset(50,50);
+            contentStream.newLineAtOffset(40, pageHeight - 30);
 
-            String text = "This is the sample document and we are adding content to it.";
+            String text = "Applitools test name and step names here";
 
             //Adding text in the form of string
             contentStream.showText(text);
